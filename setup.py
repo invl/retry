4dcec22b676e8674bfcc67a15b53a8132ca8d5c4
invlpg@gmail.com <invlpg@gmail.com>
2013-06-23 19:35:47 +0800

    Add description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(
     name='retry',
     version='0.2.0',
+    description='retry decorator',
     py_modules=['retry'],
     install_requires=[
         'decorator',
