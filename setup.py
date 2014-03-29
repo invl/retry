53dff15b53febdc2e136354c4af85822dace8031
invlpg@gmail.com <invlpg@gmail.com>
2014-03-29 12:22:44 +0800

    Version 0.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='retry',
-    version='0.3.0',
+    version='0.4.0',
     description='retry decorator',
     packages=['retry'],
     install_requires=[
