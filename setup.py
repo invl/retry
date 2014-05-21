ad457b71c8e013437073c71109874660a238aad8
invlpg@gmail.com <invlpg@gmail.com>
2014-05-21 12:35:00 +0800

    Add url

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.4.1',
     description='retry decorator',
     long_description=open('README.rst').read(),
+    url='https://github.com/invl/retry',
     packages=['retry'],
     license='Apache License 2.0',
     install_requires=[
