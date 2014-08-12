f51481e71cfd8096062f1e39509cf48e495d3e88
invlpg <invlpg@gmail.com>
2014-08-13 06:39:44 +0800

    Add test_retry.py

---
+++ test_retry.py
@@ -0,0 +1,16 @@
+import pytest
+
+from retry import retry
+
+
+def test_tries():
+    hit = [0]
+
+    @retry(tries=3, delay=1)
+    def f():
+        hit[0] += 1
+        1 / 0
+
+    with pytest.raises(ZeroDivisionError):
+        f()
+    assert hit[0] == 4
