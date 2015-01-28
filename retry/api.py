7e9fdd4e33f78d23320d113a6c4a6c81f6abd93f
invlpg <invlpg@gmail.com>
2015-01-28 15:48:32 +0800

    logger is now optional

--- retry/api.py
+++ retry/api.py
@@ -30,7 +30,8 @@ def retry(exceptions=Exception, tries=float('inf'), delay=0, backoff=1, logger=l
     :param tries: the maximum number of attempts
     :param delay: how many seconds to wait between attmpts
     :param backoff: delay growth factor
-    :param logger: logger.warning(fmt, error, delay) will be called on failed attempts
+    :param logger: logger.warning(fmt, error, delay) will be called on failed attempts.
+                   defaults to retry.logging_logger. If logger=None, logging is disabled.
     """
 
     @decorator
@@ -42,7 +43,8 @@ def retry(exceptions=Exception, tries=float('inf'), delay=0, backoff=1, logger=l
                 if i >= tries:
                     raise
                 round_delay = delay * backoff ** i
-                logger.warning('%s, retrying in %s seconds...', e, round_delay)
+                if logger is not None:
+                    logger.warning('%s, retrying in %s seconds...', e, round_delay)
                 time.sleep(round_delay)
 
     return retry_decorator
