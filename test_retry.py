5c3c322705e730cb0cd3e5ee05d65ed6b141fc4d
invlpg <invlpg@gmail.com>
2015-03-11 13:25:52 +0800

    Mock time.sleep in test case

--- test_retry.py
+++ test_retry.py
@@ -1,14 +1,25 @@
+import time
+
 import pytest
 
 from retry import retry
 
 
-def test_tries():
+def test_retry(monkeypatch):
+    mock_sleep_time = [0]
+
+    def mock_sleep(seconds):
+        mock_sleep_time[0] += seconds
+
+    monkeypatch.setattr(time, 'sleep', mock_sleep)
+
     hit = [0]
 
-    tries = 3
+    tries = 5
+    delay = 1
+    backoff = 2
 
-    @retry(tries=tries, delay=0.1)
+    @retry(tries=tries, delay=delay, backoff=backoff)
     def f():
         hit[0] += 1
         1 / 0
@@ -16,3 +27,5 @@ def test_tries():
     with pytest.raises(ZeroDivisionError):
         f()
     assert hit[0] == tries
+    assert mock_sleep_time[0] == sum(
+        delay * backoff ** i for i in range(tries - 1))
