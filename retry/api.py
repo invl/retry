399d5f19654ac434f23e1edbdac0e42c152aaed6
invlpg <invlpg@gmail.com>
2015-03-11 14:09:06 +0800

    Move decorator() to .compat

--- retry/api.py
+++ retry/api.py
@@ -1,23 +1,10 @@
-import functools
 import time
 import logging
 
 try:
     from decorator import decorator
 except ImportError:
-    def decorator(caller):
-        """ Turns caller into a decorator.
-        Unlike decorator module, function signature is not preserved.
-
-        :param caller: caller(f, *args, **kwargs)
-        """
-        def decor(f):
-            @functools.wraps(f)
-            def wrapper(*args, **kwargs):
-                return caller(f, *args, **kwargs)
-            return wrapper
-        return decor
-
+    from .compat import decorator
 
 logging_logger = logging.getLogger(__name__)
 
