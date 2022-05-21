ca057012ae695b5f67dcb9ad6853ee1b33d1cb2c
brookman1 <ystreltsov_99@yahoo.com>
2022-05-21 15:30:43 -0400

    Removed global declarations.

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -1,70 +1,120 @@
-try:
-    from unittest.mock import create_autospec
-    from unittest.mock import MagicMock
-    from unittest import mock
-except ImportError:
-    from mock import create_autospec
-    from mock import MagicMock
-    import mock
-
-import threading
-
+import asyncio
+from importlib import reload
+from unittest.mock import MagicMock
 import pytest
-
-import retry.api
-
+import time
 import logging
 
 logging_logger = logging.Logger(__name__)
 
-mock_sleep_time = [0]
+class MockCondition(object):
+    mock_sleep_time = [0]
+    def acquire(self):
+        logging_logger.warning('in acquire')
+        return True
+        
+    def release(self):
+        logging_logger.warning('in release')
+        
+    def wait(self, seconds):
+        logging_logger.warning('in wait')
+        print('\n'*10+'in wait'+'\n')
+        MockCondition.mock_sleep_time[0] += seconds
 
-class mockCondition():
-	def acquire(self):
-		logging_logger.warning('in acquire')
-		return True
-		
-	def release(self):
-		logging_logger.warning('in release')
-		
-	def wait(self, seconds):
-		logging_logger.warning('in wait')
-		mock_sleep_time[0] += seconds
+
+import threading
+threading.Condition = MockCondition
+from reretry.api import _is_async, retry, retry_call
+import reretry.api
+
+thread_loops = 0
+total_thread_loops = 1000
+
+def test_threaded_retry(monkeypatch):
+    '''
+    For this thread actual threading is used, threading condition is
+    remocked at the end.
+    '''
+    reload(threading)
+    hit = [0]
+
+    tries = 5
+    delay = 0.01
+    backoff = 2
+
+    @retry(tries=tries, delay=delay, backoff=backoff)
+    def f():
+        hit[0] += 1
+        1 / 0
+
+    def run():
+        global thread_loops
+        c = threading.Condition()
+        c.acquire()
+        thread_delay = delay / total_thread_loops
+        for a in range(total_thread_loops):
+            c.wait(thread_delay)
+            thread_loops += 1
+        c.release()
+
+    test_thread = threading.Thread(target=run)
+    test_thread.start()
+    c = threading.Condition()
+    # waiting for the test to start
+    c.acquire()
+    c.wait(.01)
+    c.release()
+
+    num_loops = thread_loops
+    with pytest.raises(ZeroDivisionError):
+        f()
+
+    num_loops = thread_loops - num_loops
+    test_thread.join()
+
+    assert num_loops > 0 and num_loops < total_thread_loops
+    threading.Condition = MockCondition
+    assert hit[0] == tries
+    assert mock_sleep_time[0] == sum(delay * backoff**i for i in range(tries - 1))
+
+
 
 
 def test_retry(monkeypatch):
-    global mock_sleep_time
-    mock_sleep_time = [0]
+    MockCondition.mock_sleep_time = [0]
 
-    monkeypatch.setattr(retry.api.threading.Condition, 'threading.Condition', mockCondition)
+    monkeypatch.setattr(reretry.api.threading, 'Condition', MockCondition)
+
     hit = [0]
 
     tries = 5
     delay = 1
     backoff = 2
 
-    @retry.api.retry(tries=tries, delay=delay, backoff=backoff, condition=mockCondition())
+    @retry(tries=tries, delay=delay, backoff=backoff)
     def f():
         hit[0] += 1
         1 / 0
+
     with pytest.raises(ZeroDivisionError):
         f()
     assert hit[0] == tries
-    assert mock_sleep_time[0] == sum(
-        delay * backoff ** i for i in range(tries - 1))
+    assert MockCondition.mock_sleep_time[0] == \
+        sum(delay * backoff**i for i in range(tries - 1))
 
 
 def test_tries_inf():
     hit = [0]
     target = 10
 
-    @retry.api.retry(tries=float('inf'), condition=mockCondition())
+    @retry(tries=float("inf"))
     def f():
         hit[0] += 1
         if hit[0] == target:
             return target
         else:
             raise ValueError
+
     assert f() == target
 
 
@@ -72,21 +122,23 @@ def test_tries_minus1():
     hit = [0]
     target = 10
 
-    @retry.api.retry(tries=-1)
+    @retry(tries=-1)
     def f():
         hit[0] += 1
         if hit[0] == target:
             return target
         else:
             raise ValueError
+
     assert f() == target
 
 
 def test_max_delay(monkeypatch):
-    global mock_sleep_time
-    mock_sleep_time = [0]
+    MockCondition.mock_sleep_time = [0]
 
-    monkeypatch.setattr(retry.api.threading.Condition, 'threading.Condition', mockCondition)
+    monkeypatch.setattr(reretry.api.threading, 'Condition', MockCondition)
+    
+    print(':: condition', reretry.api.threading.Condition)
 
     hit = [0]
 
@@ -95,7 +147,7 @@ def test_max_delay(monkeypatch):
     backoff = 2
     max_delay = delay  # Never increase delay
 
-    @retry.api.retry(tries=tries, delay=delay, max_delay=max_delay, backoff=backoff, condition=mockCondition())
+    @retry(tries=tries, delay=delay, max_delay=max_delay, backoff=backoff)
     def f():
         hit[0] += 1
         1 / 0
@@ -103,21 +155,20 @@ def test_max_delay(monkeypatch):
     with pytest.raises(ZeroDivisionError):
         f()
     assert hit[0] == tries
-    assert mock_sleep_time[0] == delay * (tries - 1)
+    assert MockCondition.mock_sleep_time[0] == delay * (tries - 1)
 
 
 def test_fixed_jitter(monkeypatch):
-    global mock_sleep_time
-    mock_sleep_time = [0]
+    MockCondition.mock_sleep_time = [0]
 
-    monkeypatch.setattr(retry.api.threading.Condition, 'threading.Condition', mockCondition)
+    monkeypatch.setattr(reretry.api.threading, 'Condition', MockCondition)
 
     hit = [0]
 
     tries = 10
     jitter = 1
 
-    @retry.api.retry(tries=tries, jitter=jitter, condition=mockCondition())
+    @retry(tries=tries, jitter=jitter)
     def f():
         hit[0] += 1
         1 / 0
@@ -125,14 +176,16 @@ def test_fixed_jitter(monkeypatch):
     with pytest.raises(ZeroDivisionError):
         f()
     assert hit[0] == tries
-    assert mock_sleep_time[0] == sum(range(tries - 1))
+    assert MockCondition.mock_sleep_time[0] == sum(range(tries - 1))
 
 
 def test_retry_call():
     f_mock = MagicMock(side_effect=RuntimeError)
     tries = 2
     try:
-        retry.api.retry_call(f_mock, exceptions=RuntimeError, tries=tries, condition=mockCondition())
+        retry_call(f_mock, exceptions=RuntimeError, tries=tries
+            , condition=MockCondition()
+        )
     except RuntimeError:
         pass
 
@@ -145,7 +198,9 @@ def test_retry_call_2():
     tries = 5
     result = None
     try:
-        result = retry.api.retry_call(f_mock, exceptions=RuntimeError, tries=tries, condition=mockCondition())
+        result = retry_call(f_mock, exceptions=RuntimeError, tries=tries
+            , condition=MockCondition()
+        )
     except RuntimeError:
         pass
 
@@ -154,7 +209,6 @@ def test_retry_call_2():
 
 
 def test_retry_call_with_args():
-
     def f(value=0):
         if value < 0:
             return value
@@ -165,7 +219,9 @@ def test_retry_call_with_args():
     result = None
     f_mock = MagicMock(spec=f, return_value=return_value)
     try:
-        result = retry.api.retry_call(f_mock, fargs=[return_value], condition=mockCondition())
+        result = retry_call(f_mock, fargs=[return_value]
+            , condition=MockCondition()
+        )
     except RuntimeError:
         pass
 
@@ -174,20 +230,91 @@ def test_retry_call_with_args():
 
 
 def test_retry_call_with_kwargs():
-
     def f(value=0):
         if value < 0:
             return value
         else:
             raise RuntimeError
 
-    kwargs = {'value': -1}
+    kwargs = {"value": -1}
     result = None
-    f_mock = MagicMock(spec=f, return_value=kwargs['value'])
+    f_mock = MagicMock(spec=f, return_value=kwargs["value"])
     try:
-        result = retry.api.retry_call(f_mock, fkwargs=kwargs, condition=mockCondition())
+        result = retry_call(f_mock, fkwargs=kwargs
+            , condition=MockCondition()
+        )
     except RuntimeError:
         pass
 
-    assert result == kwargs['value']
+    assert result == kwargs["value"]
     assert f_mock.call_count == 1
+
+
+def test_retry_call_with_fail_callback():
+    def f():
+        raise RuntimeError
+
+    def cb(error):
+        pass
+
+    callback_mock = MagicMock(spec=cb)
+    try:
+        retry_call(f, fail_callback=callback_mock, tries=2
+            , condition=MockCondition()
+        )
+    except RuntimeError:
+        pass
+
+    assert callback_mock.called
+
+
+def test_is_async():
+    async def async_func():
+        pass
+
+    def non_async_func():
+        pass
+
+    def generator():
+        yield
+
+
+    assert _is_async(async_func)
+    assert not _is_async(non_async_func)
+    assert not _is_async(generator)
+    assert not _is_async(generator())
+    assert not _is_async(MagicMock(spec=non_async_func, return_value=-1))
+
+
+@pytest.mark.asyncio
+async def test_async():
+    attempts = 1
+    raised = False
+
+    @retry(tries=2)
+    async def f():
+        await asyncio.sleep(0.1)
+        nonlocal attempts, raised
+        if attempts:
+            raised = True
+            attempts -= 1
+            raise RuntimeError
+        return True
+
+    assert await f()
+    assert raised
+    assert attempts == 0
+
+
+def test_check_params():
+    with pytest.raises(AssertionError):
+        retry_call(lambda: None, show_traceback=True, logger=None)
+
+    async def async_func():
+        pass
+
+    def non_async_func():
+        pass
+
+    with pytest.raises(AssertionError):
+        retry_call(async_func, fail_callback=non_async_func)
